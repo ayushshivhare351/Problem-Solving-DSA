581fbe12d3d2374935a83d02c27f2d744a79ab82
Ayush Shivhare <ayushshivhare351@gmail.com>
2026-07-16 21:31:28 +0530

    Time: 20 ms (18.21%) | Memory: 20.5 MB (24.36%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,12 +1,22 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
+        from heapq import heappush,heappop,heapify
+        heap = []
+        heapify_max(heap)
         freq = {}
         for item in s:
-            freq[item] = freq.get(item,0)+1
-        mx = max(freq.values())
+            freq[item]= freq.get(item,0)+1
+
+        for item in freq:
+            heappush_max(heap,[freq[item],item])
+
         res = ""
-        for i in range(mx,0,-1):
-            for ch in freq:
-                if freq[ch]== i:
-                    res += ch*i
+        while heap:
+            temp = []
+            ss = ""
+            a = heappop_max(heap)
+            for i in range(a[0]):
+                ss+= a[1]
+            res+=ss
         return res
+        
